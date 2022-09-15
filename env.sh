e158ed5ea15105a9915ee20f14cbd950eee04947
Admas Kinfu <akinfu@chegg.com>
2022-09-14 17:41:21 -0700

    initial commit with docker-compose

---
+++ env.sh
@@ -0,0 +1,3 @@
+export WORDPRESS_DB_USER=<some user name. this user will automaticaly be created>
+export WORDPRESS_DB_PASSWORD=<some password>
+export WORDPRESS_DB_NAME=<name for datablse of the site. this database will created>
